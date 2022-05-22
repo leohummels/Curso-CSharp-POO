8885197418db5ddb3e1421ed89c54f700965a36a
leohummels <lsousa767@gmail.com>
2022-05-22 20:09:25 -0300

    Adicionar arquivos de projeto.

---
+++ bytebank/ContaCorrente.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using bytebank.Titular;
+
+namespace bytebank
+{
+    public class ContaCorrente
+    {
+        //propriedade autoimplementada
+        public Cliente Titular{get;set;}
+        private string _conta;
+        public string Conta
+        {
+            get
+            {
+                return _conta;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    return;
+                }
+                else
+                {
+                    _conta = value;
+                }
+            }
+        }
+        private int _numero_Agencia;
+
+        public int Numero_Agencia { 
+            get
+            {
+                return _numero_Agencia;
+            }
+            set
+            {
+                if(_numero_Agencia <= 0)
+                {
+                    return;
+                }
+                else
+                {
+                    _numero_Agencia = value;
+                }
+            }
+        }
+
+        public string Nome_Agencia { get; set; }
+        public double saldo;
+
+       public bool Sacar(double valor)
+        {
+            if(saldo < valor)
+            {
+                return false;
+            } else if(valor <  0)
+            {
+                return false;
+            } else
+            {
+                saldo -= valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente destino)
+        {
+            if(saldo < valor)
+            {
+                return false;
+            } else if (valor < 0)
+            {
+                return false;
+            } else
+            {
+                saldo = saldo - valor;
+                destino.saldo = destino.saldo - valor;
+                return true;
+            }
+        }
+
+        public void ExibirDadosDaConta()
+        {
+            Console.WriteLine("Titular: " + Titular);
+            Console.WriteLine("Conta: " + Conta);
+            Console.WriteLine("Número da agência: " + Numero_Agencia);
+            Console.WriteLine("Nome da Agência: " + Nome_Agencia);
+            Console.WriteLine("Saldo: " + saldo);
+        }
+
+        public double Saldo
+        {
+            get {
+                return saldo;
+            }
+            set { 
+                if(value < 0)
+                {
+                    Saldo = value;
+                }
+            }
+        }   
+
+        public double GetSaldo()
+        {
+            return saldo;
+        }
+
+        public ContaCorrente(int numero_Agencia, string conta)
+        {
+            Numero_Agencia = numero_Agencia;
+            Conta = conta;
+            TotalDeContasCriadas += 1;
+        }
+
+        public static int TotalDeContasCriadas { get; set; }
+    }
+}
---
+++ bytebank/Program.cs
@@ -0,0 +1,48 @@
+﻿using bytebank;
+using bytebank.Titular;
+
+Console.WriteLine("Bem vindo ao seu banco!!");
+
+//ContaCorrente conta1 = new ContaCorrente();
+
+//conta1.titular = "Leozin da Verdinha";
+//conta1.conta = "10123-x";
+//conta1.numero_agencia = 23;
+//conta1.nome_agencia = "Agencia Central";
+//conta1.saldo = 9500.46;
+
+//ContaCorrente conta2 = new ContaCorrente();
+//conta2.titular = "Amanda Nogueira";
+//conta2.conta = "10106-X";
+//conta2.numero_agencia = 321;
+//conta2.nome_agencia = "Agência Central";
+//conta2.saldo = 1000;
+
+//conta1.ExibirDadosDaConta();
+
+//Cliente cliente = new Cliente();
+//cliente.Proffissao = "programador";
+//cliente.Nome = "Leozin do C#";
+//cliente.CPF = "Fake CPF";
+
+//ContaCorrente conta3 = new ContaCorrente(32, "0000");
+//conta3.Titular = cliente;
+//conta3.Nome_Agencia = "Central";
+
+
+//Ao criar uma classe, e atribuir propriedades a ela, temos valores padrões quando não os definimos
+
+//Console.WriteLine("Saldo da Amanda: " + conta2.saldo);
+//Console.WriteLine("Saldo do Leozin: " + conta1.saldo);
+
+//bool transferencia = conta1.Transferir(450, conta2);
+//Console.WriteLine("Transferncia com sucesso:  " + transferencia);
+
+//Console.WriteLine("Saldo da Amanda: " + conta2.saldo);
+//Console.WriteLine("Saldo do Leozin: " + conta1.saldo);
+
+ContaCorrente conta5 = new ContaCorrente(123, "000001");
+ContaCorrente conta6 = new ContaCorrente(768, "000011");
+
+Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+Console.ReadKey();
